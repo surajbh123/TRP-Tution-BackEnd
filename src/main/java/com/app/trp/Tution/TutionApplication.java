2a1e2c0776be05c93e330ee59c0a8e047fc40f42
vishalmunagekar <vishalmunagekar@gmail.com>
2021-04-20 13:35:02 +0530

    Spring Boot initial application

---
+++ src/main/java/com/app/trp/Tution/TutionApplication.java
@@ -0,0 +1,13 @@
+package com.app.trp.Tution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/trp/Tution/TutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.trp.Tution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
